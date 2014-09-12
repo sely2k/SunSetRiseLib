e826a76c88229e3f8d0fe071590e5515f246036c
LEONARDO DOttavi <leonardo.dottavi@me.com>
2014-09-12 18:13:39 +0200

    #1 firts commit of code from http://pointofint.blogspot.it/2014/06/sunrise-and-sunset-in-c.html

---
+++ TestConsole/TestConsole.v4.5.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using SunSetRiseLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole.v4._5._1
+{
+    class Program
+    {
+        static double JD = 0;
+        static int zone = -8; // Seattle time Zone
+        static double latitude = 47.6; // Seattle lat
+        static double longitude = -122.32; // Seattle lon 
+        static bool dst = true; // Day Light Savings 
+        static DateTime date = DateTime.Today;
+
+        static void Main(string[] args)
+        {
+
+            JD = Util.calcJD(date);  //OR   JD = Util.calcJD(2014, 6, 1);
+            double sunRise = Util.calcSunRiseUTC(JD, latitude, longitude);
+            double sunSet = Util.calcSunSetUTC(JD, latitude, longitude);
+            System.Console.WriteLine(Util.getTimeString(sunRise, zone, JD, dst));
+            System.Console.WriteLine(Util.getTimeString(sunSet, zone, JD, dst));
+            System.Console.WriteLine("");
+            System.Console.WriteLine(Util.getDateTime(sunRise, zone, date, dst).Value.ToString());
+            System.Console.WriteLine(Util.getDateTime(sunSet, zone, date, dst).Value.ToString());
+        }
+    }
+}
